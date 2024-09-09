d4cfa2033fde5265109b31850ac28fdf7c156a4d
Karim Gargouri <karim.gargouri@polymtl.ca>
2024-09-09 13:08:33 -0400

    Automated update for task: add a new endpoint be creative

--- hello.py
+++ hello.py
@@ -1,12 +1,15 @@
 # Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
 
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/greet/<name>')
+def greet(name):
+    return jsonify(message=f'Hello, {name}! Welcome to our API.')
+
 if __name__ == '__main__':
     app.run()
-
