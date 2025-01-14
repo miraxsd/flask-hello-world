76a4c834581d97b94251e77b4446d90beefed93c
root <root@c7b290a8c3a2>
2025-01-14 01:26:19 +0000

    add an endpoint to convert celcius to farhaneit

--- hello.py
+++ hello.py
@@ -1,11 +1,19 @@
 # Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/celsius_to_fahrenheit')
+def celsius_to_fahrenheit():
+    celsius = request.args.get('celsius', type=float)
+    if celsius is None:
+        return "Please provide a 'celsius' query parameter", 400
+    fahrenheit = (celsius * 9/5) + 32
+    return f"{celsius} degrees Celsius is equivalent to {fahrenheit} degrees Fahrenheit."
 
 if __name__ == '__main__':
     app.run()
