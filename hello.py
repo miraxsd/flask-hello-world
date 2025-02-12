2e7457f28dd775061db2fcc99352337ababe13a4
Karim Gargouri <karim.gargouri@polymtl.ca>
2025-02-11 19:30:54 -0500

    Automated update for task: Add a new endpoint to convert celcius to ferheneit

--- hello.py
+++ hello.py
@@ -1,12 +1,19 @@
 # Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
 
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/convert', methods=['GET'])
+def convert_celsius_to_fahrenheit():
+    celsius = request.args.get('celsius', type=float)
+    if celsius is None:
+        return jsonify({'error': 'Please provide a Celsius value.'}), 400
+    fahrenheit = (celsius * 9/5) + 32
+    return jsonify({'celsius': celsius, 'fahrenheit': fahrenheit})
+
 if __name__ == '__main__':
     app.run()
-
