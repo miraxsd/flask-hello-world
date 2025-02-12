2909b2ce73cf1c85a3113b6ab65235a959dd10e4
Karim Gargouri <karim.gargouri@polymtl.ca>
2025-02-11 19:53:51 -0500

    Automated update for task: Additional Information: Add another endpoint to convert Farhaneit to celcius

--- hello.py
+++ hello.py
@@ -15,5 +15,13 @@ def convert_celsius_to_fahrenheit():
     fahrenheit = (celsius * 9/5) + 32
     return jsonify({'celsius': celsius, 'fahrenheit': fahrenheit})
 
+@app.route('/convert_fahrenheit', methods=['GET'])
+def convert_fahrenheit_to_celsius():
+    fahrenheit = request.args.get('fahrenheit', type=float)
+    if fahrenheit is None:
+        return jsonify({'error': 'Please provide a valid Fahrenheit value.'}), 400
+    celsius = (fahrenheit - 32) * 5/9
+    return jsonify({'fahrenheit': fahrenheit, 'celsius': celsius})
+
 if __name__ == '__main__':
     app.run()
