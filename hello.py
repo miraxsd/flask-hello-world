8e7e53a7dc965d45bcdab76c8358a3a586fd20ff
Karim Gargouri <karim.gargouri@polymtl.ca>
2025-02-15 20:59:02 -0500

    Automated update for task: Add an endpoint to convert time in hours to seconds

--- hello.py
+++ hello.py
@@ -1,12 +1,19 @@
 # Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
 
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/convert_time', methods=['GET'])
+def convert_time():
+    hours = request.args.get('hours', type=float)
+    if hours is None:
+        return jsonify({'error': 'Please provide hours as a query parameter.'}), 400
+    seconds = hours * 3600
+    return jsonify({'seconds': seconds})
+
 if __name__ == '__main__':
     app.run()
-
