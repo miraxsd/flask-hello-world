6b994e273f66be83a4852c08a4290c0ee74809b0
Karim Gargouri <karim.gargouri@polymtl.ca>
2025-02-19 22:22:54 -0500

    Automated update for task: add an endpoint to convert numbers to their equivalent text

--- hello.py
+++ hello.py
@@ -1,12 +1,22 @@
 # Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
 
-from flask import Flask
+from flask import Flask, request, jsonify
+import inflect
+
 app = Flask(__name__)
+p = inflect.engine()
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/convert', methods=['GET'])
+def convert_number_to_text():
+    number = request.args.get('number', type=int)
+    if number is None:
+        return jsonify({'error': 'No number provided'}), 400
+    text = p.number_to_words(number)
+    return jsonify({'number': number, 'text': text})
+
 if __name__ == '__main__':
     app.run()
-
