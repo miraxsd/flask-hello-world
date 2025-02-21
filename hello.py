a36462418b61254c2191b2e8e8a0eee68574cfc7
Karim Gargouri <karim.gargouri@polymtl.ca>
2025-02-20 20:06:13 -0500

    Automated update for task: ADD a new endpoint to convert minutes to seconds and add a file to test this new feat

--- hello.py
+++ hello.py
@@ -1,12 +1,17 @@
 # Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
 
-from flask import Flask
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/convert_minutes_to_seconds', methods=['GET'])
+def convert_minutes_to_seconds():
+    minutes = request.args.get('minutes', default=0, type=int)
+    seconds = minutes * 60
+    return jsonify({'minutes': minutes, 'seconds': seconds})
+
 if __name__ == '__main__':
     app.run()
-
