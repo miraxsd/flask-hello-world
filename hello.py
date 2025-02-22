ac11b3b8fcd9883ab011ec366fb677b1507d5c90
Karim Gargouri <karim.gargouri@polymtl.ca>
2025-02-21 21:38:11 -0500

    Automated update for task: Add an endpoint to make me laugh, add tests and add a readme for the project

--- hello.py
+++ hello.py
@@ -1,12 +1,24 @@
 # Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
 
-from flask import Flask
+from flask import Flask, jsonify
+import random
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/laugh')
+def make_me_laugh():
+    jokes = [
+        "Why don't scientists trust atoms? Because they make up everything!",
+        "Why did the scarecrow win an award? Because he was outstanding in his field!",
+        "I told my wife she was drawing her eyebrows too high. She looked surprised!",
+        "Why don't skeletons fight each other? They don't have the guts!",
+        "What do you call fake spaghetti? An impasta!"
+    ]
+    return jsonify({'joke': random.choice(jokes)})
+
 if __name__ == '__main__':
     app.run()
-
