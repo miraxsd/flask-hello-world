0248ab01de61ac2c6b70f7e68b033efd8222472f
Karim Gargouri <karim.gargouri@polymtl.ca>
2025-02-24 19:10:49 -0500

    Automated update for task: Add an endpoint to make me laugh Previous changes: None

--- hello.py
+++ hello.py
@@ -1,12 +1,40 @@
 # Shamelessly copied from http://flask.pocoo.org/docs/quickstart/
 
-from flask import Flask
+from flask import Flask, jsonify
+import random
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def hello_world() -> str:
+    """Return a friendly greeting.
+
+    Returns:
+        str: A greeting message.
+    """
     return 'Hello World!'
+
+@app.route('/laugh')
+def make_me_laugh() -> str:
+    """Return a random joke to make you laugh.
+
+    Returns:
+        str: A random joke.
+
+    Raises:
+        Exception: If there is an issue retrieving a joke.
+    """
+    jokes: list[str] = [
+        "Why don't scientists trust atoms? Because they make up everything!",
+        "Why did the scarecrow win an award? Because he was outstanding in his field!",
+        "I told my wife she was drawing her eyebrows too high. She looked surprised!",
+        "Why don't skeletons fight each other? They don't have the guts!"
+    ]
+
+    try:
+        return random.choice(jokes)
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
 
 if __name__ == '__main__':
     app.run()
-
